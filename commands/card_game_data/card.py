4d720e0f91f8f1d4dde43787f8abe1bb3044f0c0
BachNob <107260269+SussyGuy35@users.noreply.github.com>
2023-05-21 18:02:13 +0700

    rebalance rps minigame

--- commands/card_game_data/card.py
+++ commands/card_game_data/card.py
@@ -2,25 +2,22 @@
 
 # Common
 card_common = ["SusHuman", "TrollerGuy", "CementCat", "HypeCat", "BuffaloKid", "DripGoci", "BachKhoa96", "RosemaryRickRoll", "AdvancedBaller", "sedcfyhjuc",
-               "DrugKid", "FunkyDunkyYummy", "Dat400MIL", "ButtSlapper", "Avis", "Aberto", "Anapned", "Sectumsempra", "Incarcarous"]
+               "DrugKid", "FunkyDunkyYummy", "Dat400MIL", "ButtSlapper", "Avis", "Aberto", "Anapned", "Sectumsempra", "Incarcarous", "AmumuMogus", "IssacMabol"] 
 # Uncommon
 card_uncommon = ["SusMonster", "PrankKid", "CandyKid", "FreeFireKid", "RacingBoy", "ChingChing", "Unicorn20cm", "LazyFrox",
-                 "UhmActually...", "Colloportus", "Babbling", "Ferula", "Penestrator", "Confringo", "Sonorus"]
+                 "UhmActually...", "Colloportus", "Babbling", "Ferula", "Penestrator", "Confringo", "Sonorus"] 
 # Rare
 card_rare = ["SushiBoi69", "TrollFace", "RubberbandBeast", "Mr.Thuong", "TikTokKid", "NinjaLead", "NyanNyanMeow", "HaxKing", "DaRoku",
-             "FreeWater", "Wingardium", "Stupefy", "Deletrius", "Finite", "Langlock", "Levicorpus", "SheenHa"]
+             "FreeWater", "Wingardium", "Stupefy", "Deletrius", "Finite", "Langlock", "Levicorpus", "SheenHa", "Mazzon"] 
 # Epic
 card_epic = ["SusLord", "TrollerMaster", "FuHoKing", "LoveCastle", "BeXuanMikeLonTon", "TouchGrass", "CoderThanSau", "Minnn",
-             "NoEscapeBread", "AnhHungHaoHan", "Patrona", "Episkey", "Gubraithian", "DongHT", "MasterOfTroll"]
+             "NoEscapeBread", "AnhHungHaoHan", "Patrona", "Episkey", "Gubraithian", "DongHT", "MasterOfTroll"] 
 # Legendary
 card_legendary = ["SussyGod", "Eden88", "BachNob", "ThengTheTriLord", "ConMeoNguNgocDangYeuCutePhoMaiQue",
-                  "10DiemVan", "BallerMaster", "Illuminati"]
+                  "10DiemVan", "BallerMaster", "Illuminati", "Alonma"] 
 
 ############### Cards class for Rock Paper Scissors ###############
 
-scissors = ['Penestrator', 'UhmActually...', 'Patrona', 'HaxKing', 'sedcfyhjuc', 'Unicorn20cm', 'ChingChing', 'DripGoci', 'Confringo', 'TrollFace', 'FreeFireKid', 'ThengTheTriLord',
-            'Colloportus', 'RosemaryRickRoll', 'NoEscapeBread', 'Sonorus', 'DongHT', 'CoderThanSau', 'LoveCastle', 'ConMeoNguNgocDangYeuCutePhoMaiQue', 'Sectumsempra', 'Episkey', 'Babbling', 'DrugKid']
-rock = ['AnhHungHaoHan', 'Eden88', 'DaRoku', 'Anapned', 'SusLord', 'BachKhoa96', 'TrollerGuy', 'Finite', 'Incarcarous', 'RacingBoy', 'HypeCat', 'SheenHa', 'CandyKid',
-        'TrollerMaster', 'BachNob', 'Langlock', 'Mr.Thuong', 'TikTokKid', 'Aberto', 'Deletrius', 'FunkyDunkyYummy', 'Avis', 'Illuminati', 'Dat400MIL', 'Levicorpus']
-paper = ['FuHoKing', 'CementCat', 'PrankKid', 'SusMonster', 'LazyFrox', 'SusHuman', 'BeXuanMikeLonTon', 'ButtSlapper', 'Minnn', 'TouchGrass', 'Wingardium', 'NinjaLead', 'NyanNyanMeow',
-         'RubberbandBeast', 'FreeWater', 'AdvancedBaller', 'Stupefy', 'BuffaloKid', '10DiemVan', 'Gubraithian', 'SussyGod', 'BallerMaster', 'Ferula', 'SushiBoi69', 'MasterOfTroll']
+scissors = ["SusHuman", "TrollerGuy", "CementCat", "HypeCat", "BuffaloKid", "DripGoci","SusMonster", "PrankKid", "CandyKid", "FreeFireKid", "RacingBoy","SushiBoi69", "TrollFace", "RubberbandBeast", "Mr.Thuong", "TikTokKid","SusLord", "TrollerMaster", "FuHoKing", "LoveCastle", "BeXuanMikeLonTon","SussyGod", "Eden88","Incarcarous","Illuminati","Mazzon"]
+rock = ["BachKhoa96", "RosemaryRickRoll", "AdvancedBaller", "sedcfyhjuc","DrugKid", "FunkyDunkyYummy","ChingChing", "Unicorn20cm", "LazyFrox","UhmActually...", "Colloportus","NinjaLead", "NyanNyanMeow", "HaxKing", "DaRoku","FreeWater","TouchGrass", "CoderThanSau", "Minnn","NoEscapeBread", "AnhHungHaoHan","BachNob", "ThengTheTriLord","BallerMaster","Levicorpus","AmumuMogus"]
+paper = ["Dat400MIL", "ButtSlapper", "Avis", "Aberto", "Anapned", "Sectumsempra","Babbling", "Ferula", "Penestrator", "Confringo", "Sonorus","Wingardium", "Stupefy", "Deletrius", "Finite", "Langlock","Patrona", "Episkey", "Gubraithian", "DongHT", "MasterOfTroll","ConMeoNguNgocDangYeuCutePhoMaiQue","10DiemVan","SheenHa","IssacMabol","Alonma"]
