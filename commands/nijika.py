928b533f070fd40c0bdf01dc54843e9b2bdbb7d0
BachNob <107260269+SussyGuy35@users.noreply.github.com>
2023-08-09 22:24:47 +0700

    more nijika
    add 8 nijika images

--- commands/nijika.py
+++ commands/nijika.py
@@ -12,7 +12,15 @@ nijika_images = [
     "data/nijika/nijika3.jpg",
     "data/nijika/nijika4.jpg",
     "data/nijika/nijika5.png",
-    "data/nijika/nijika6.png"
+    "data/nijika/nijika6.png",
+    "data/nijika/nijika7.gif",
+    "data/nijika/nijika8.png",
+    "data/nijika/nijika9.jpg",
+    "data/nijika/nijika10.jpg",
+    "data/nijika/nijika11.jpg",
+    "data/nijika/nijika12.jpg",
+    "data/nijika/nijika13.jpg",
+    "data/nijika/nijika14.jpg"
 ]
 
 def command_response():
