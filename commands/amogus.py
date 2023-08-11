3f00a245c60da9378c93e57e5e62ec9012e7721f
BachNob <107260269+SussyGuy35@users.noreply.github.com>
2023-08-11 19:28:51 +0700

    update amogus command and nijika command to load image dynamic

--- commands/amogus.py
+++ commands/amogus.py
@@ -2,17 +2,10 @@ import discord, random, os
 
 base_path = os.path.dirname(os.path.abspath(__file__))
 
+imgpath = "data/susimg/"
+
 def absolute_path(relative_path):
     return os.path.join(base_path,relative_path)
 
-
-amogusImg = [
-    "data/susimg/amogus.jpg",
-    "data/susimg/amogusB.gif",
-    "data/susimg/amogusDiscord.gif",
-    "data/susimg/amogusdrip.jpg",
-    "data/susimg/pysus.png",
-]
-
 def command_response():
-    return discord.File(absolute_path(random.choice(amogusImg)))
+    return discord.File(absolute_path(imgpath + random.choice(os.listdir(absolute_path(imgpath)))))
--- commands/nijika.py
+++ commands/nijika.py
@@ -2,26 +2,10 @@ import discord, random, os
 
 base_path = os.path.dirname(os.path.abspath(__file__))
 
+imgpath = "data/nijika/"
+
 def absolute_path(relative_path):
     return os.path.join(base_path,relative_path)
 
-
-nijika_images = [
-    "data/nijika/nijika1.jpg",
-    "data/nijika/nijika2.jpg",
-    "data/nijika/nijika3.jpg",
-    "data/nijika/nijika4.jpg",
-    "data/nijika/nijika5.png",
-    "data/nijika/nijika6.png",
-    "data/nijika/nijika7.gif",
-    "data/nijika/nijika8.png",
-    "data/nijika/nijika9.jpg",
-    "data/nijika/nijika10.jpg",
-    "data/nijika/nijika11.jpg",
-    "data/nijika/nijika12.jpg",
-    "data/nijika/nijika13.jpg",
-    "data/nijika/nijika14.jpg"
-]
-
 def command_response():
-    return discord.File(absolute_path(random.choice(nijika_images)))
+    return discord.File(absolute_path(imgpath + random.choice(os.listdir(absolute_path(imgpath)))))
