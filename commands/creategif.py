0a8e2171d4982b31160b1447f9e144b5a7bd2dc8
SussyGuy35 <tqbach184@gmail.com>
2023-10-17 17:55:59 +0700

    improve create_gif command

--- commands/creategif.py
+++ commands/creategif.py
@@ -4,13 +4,13 @@ from requests import get
 
 def get_file(url, file_name):
     """Get a file from given url. Return file's name if the request was successful, None if not."""
-    # Send a GET request to the URL
-    response = get(url)
-    # Check if the request was successful
-    if response.status_code == 200:
-        # Get the content of the response (i.e., the file data)
-        file_data = response.content
-        if file_name.split(".")[-1] in ["png", "jpg", "webp", "bmp"]:
+    if file_name.split(".")[-1] in ["png", "jpg", "webp", "bmp"]:
+        # Send a GET request to the URL
+        response = get(url)
+        # Check if the request was successful
+        if response.status_code == 200:
+            # Get the content of the response (i.e., the file data)
+            file_data = response.content    
             new_file_name = file_name[:-len(file_name.split(".")[-1])]+"gif"
         else:
             return None
