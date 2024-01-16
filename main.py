a48765f88432b0e663254ce03937462286c60985
SussyGuy35 <tqbach184@gmail.com>
2024-01-16 21:14:41 +0700

    now you can only gvs on text or voice channel, not private message

--- main.py
+++ main.py
@@ -402,18 +402,19 @@ async def on_message(message: discord.Message):
            
     else:
         if not message.author.bot:
-            # gvs
-            if "gvs" in message.content.lower():
-                gvs.gvs(userid, username, str(message.guild.id))
-        
-            # Auto react emojis
-            for word, emojis in autoreact_emojis.items():
-                if word in message.content.lower():
-                    for emoji in emojis:
-                        try:
-                            await message.add_reaction(emoji)
-                        except:
-                            pass
-                    break
+            if message.channel.type == discord.ChannelType.text or message.channel.type == discord.ChannelType.voice:
+                # gvs
+                if "gvs" in message.content.lower():
+                    gvs.gvs(userid, username, str(message.guild.id))
+            
+                # Auto react emojis
+                for word, emojis in autoreact_emojis.items():
+                    if word in message.content.lower():
+                        for emoji in emojis:
+                            try:
+                                await message.add_reaction(emoji)
+                            except:
+                                pass
+                        break
 
 client.run(TOKEN)
