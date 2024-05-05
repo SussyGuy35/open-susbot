77fa4d0cf363f327620677cd6ca9ca9e6a13cb33
SussyGuy35 <tqbach184@gmail.com>
2024-05-05 20:49:59 +0700

    fancy code

--- config.py
+++ config.py
@@ -7,26 +7,26 @@ TOKEN = 'ur bot token here'
 OSUAPI_CLIENT_ID = 'ur osu!api client id here'
 OSUAPI_CLIENT_SECRET = 'ur osu!api client secret here'
 
-language = "en" # bot's language
+language = "en"  # bot's language
 
-banned_users = [] # place banned users's id here!
+banned_users = []  # place banned users' id here!
 
 specific_prefix = {
-} # Place custom prefix for specific server here in form "serverid: prefix"
+}  # Place custom prefix for specific server here in form "server_id: prefix"
 # Example:
 # specific_prefix = {
 # 69420105727: "n!"
 # }
 
 autoreact_emojis = {
-} # place autoreact emojis here in form "word: emoji"
+}  # place autoreact emojis here in form "word: emoji"
 # Example:
 # autoreact_emojis = {
 #   "gvs": ["🇬","🇰","🇪","🇻","🇦","🇾","🇸","🅰️","🇴","😳"],
 #   "tin chuan chua anh": ["🇯", "🇺", "🇦", "🇳"]
 # }
 
-enable_ghostping_detector = False # enable ghostping detector or not
-ghostping_check_time_range = 15 # time to detect ghostping
-ghostping_detector_blacklist_guild = [] # place id of servers that dont use ghostping detector here!
-ghostping_detector_blacklist_user = [] # place id of users that should not be warn by the ghostping detector here!
+enable_ghostping_detector = False  # enable ghostping detector or not
+ghostping_check_time_range = 15  # time to detect ghostping
+ghostping_detector_blacklist_guild = []  # place id of servers that don't use ghostping detector here!
+ghostping_detector_blacklist_user = []  # place id of users that should not be warned by the ghostping detector here!
--- main.py
+++ main.py
@@ -3,18 +3,6 @@ import os
 from lib.locareader import get_string_by_id
 from lib.sussyutils import get_prefix
 
-if os.path.exists("config_override.py"):
-    import config_override as config
-else:
-    import config
-
-# Config
-bot_version = config.bot_version
-
-TOKEN = config.TOKEN
-
-print(f'{config.bot_name} v{bot_version}')
-
 # import commands
 from commands import (
     amogus, ask, creategif, echo, emoji as getemoji,
@@ -30,6 +18,18 @@ import features.auto_react_emoji
 import features.gvscount
 import features.on_bot_mentioned
 
+if os.path.exists("config_override.py"):
+    import config_override as config
+else:
+    import config
+
+# Config
+bot_version = config.bot_version
+
+TOKEN = config.TOKEN
+
+print(f'{config.bot_name} v{bot_version}')
+
 intents = discord.Intents.default()
 intents.message_content = True
 
@@ -37,219 +37,206 @@ client = discord.Client(intents=intents)
 
 tree = discord.app_commands.CommandTree(client)
 
-# autoreact emojis
 autoreact_emojis = config.autoreact_emojis
 
-# loca thing
-def get_string(id: str, loca: str = "main"):
-    return get_string_by_id(f"loca/loca - {loca}.csv",id,config.language)
 
-### Slash command
-# Feedback
-@tree.command(name = "feedback", description = get_string("command_feedback_desc"))
+# loca thing
+def get_string(id_: str, loca: str = "main"):
+    return get_string_by_id(f"loca/loca - {loca}.csv", id_, config.language)
+
+
+# --- Slash command ---
+
+@tree.command(name="feedback", description=get_string("command_feedback_desc"))
 async def send_feedback(ctx: discord.Interaction):
     await feedback.slash_command_listener(ctx)
 
-# Help
-@tree.command(name = "help", description = get_string("command_help_desc")) 
-async def help(ctx: discord.Interaction):
+
+@tree.command(name="help", description=get_string("command_help_desc"))
+async def get_help(ctx: discord.Interaction):
     await bot_help.slash_command_listener(ctx)
 
-# Ping
-@tree.command(name = "ping", description = get_string("command_ping_desc")) 
+
+@tree.command(name="ping", description=get_string("command_ping_desc"))
 async def pingpong(ctx: discord.Interaction):
     await ping.slash_command_listener(ctx, client)
 
-# Avatar
-@tree.command(name = "avatar", description = get_string("command_avatar_desc")) 
-async def get_avatar(ctx: discord.Interaction,user:discord.User,server_avatar:bool = True):
+
+@tree.command(name="avatar", description=get_string("command_avatar_desc"))
+async def get_avatar(ctx: discord.Interaction, user: discord.User, server_avatar: bool = True):
     await avatar.slash_command_listener(ctx, user, server_avatar)
 
-# Emoji
-@tree.command(name = "emoji", description = get_string("command_emoji_desc")) 
-async def get_emoji(ctx: discord.Interaction, emoj: str):
-    await getemoji.slash_command_listener(client, ctx, emoj)
 
-# Nijika command
-@tree.command(name = "nijika", description = get_string("command_nijika_desc"))
+@tree.command(name="emoji", description=get_string("command_emoji_desc"))
+async def get_emoji(ctx: discord.Interaction, emoji: str):
+    await getemoji.slash_command_listener(client, ctx, emoji)
+
+
+@tree.command(name="nijika", description=get_string("command_nijika_desc"))
 async def get_nijika_image(ctx: discord.Interaction):
     await nijika.slash_command_listener(ctx)
 
-#Amogus command
-@tree.command(name = "amogus", description = get_string("command_amogus_desc"))
+
+@tree.command(name="amogus", description=get_string("command_amogus_desc"))
 async def get_amogus_image(ctx: discord.Interaction):
     await amogus.slash_command_listener(ctx)
 
-# osu user
-@tree.command(name = "osu_user", description = get_string("command_osu_user_desc")) 
+
+@tree.command(name="osu_user", description=get_string("command_osu_user_desc"))
 async def osu_user(ctx: discord.Interaction, username: str):
     await osu.slash_command_listener_user(ctx, username)
 
-# osu beatmap
-@tree.command(name = "osu_beatmap", description = get_string("command_osu_beatmap_desc")) 
+
+@tree.command(name="osu_beatmap", description=get_string("command_osu_beatmap_desc"))
 async def osu_beatmap(ctx: discord.Interaction, beatmap: str):
     await osu.slash_command_listener_beatmap(ctx, beatmap)
 
-# gvs count
-@tree.command(name = "gvs_count", description = get_string("command_gvs_count_desc"))
+
+@tree.command(name="gvs_count", description=get_string("command_gvs_count_desc"))
 async def gvs_count(ctx: discord.Interaction):
     await gvs.slash_command_listener_count(ctx)
 
-# gvs lb
-@tree.command(name = "gvs_leaderboard", description = get_string("command_gvs_leaderboard_desc"))
+
+@tree.command(name="gvs_leaderboard", description=get_string("command_gvs_leaderboard_desc"))
 async def gvs_lb(ctx: discord.Interaction):
     await gvs.slash_command_listener_lb(ctx)
 
-# gvs react
-@tree.command(name = "gvs_react", description = get_string("command_gvs_react_desc"))
+
+@tree.command(name="gvs_react", description=get_string("command_gvs_react_desc"))
 async def gvs_react(ctx: discord.Interaction, message_id: str | None = None):
     await gvs.slash_command_listener_react_message(ctx, message_id)
 
-# random cat girl
-@tree.command(name = "randcat", description = get_string("command_randcat_desc"))
-async def getrandcat(ctx: discord.Interaction, is_cat_girl:bool = False):
+
+@tree.command(name="randcat", description=get_string("command_randcat_desc"))
+async def get_randcat(ctx: discord.Interaction, is_cat_girl: bool = False):
     await randcat.slash_command_listener(ctx, is_cat_girl)
 
-# random waifu
-@tree.command(name = "randwaifu", description = get_string("command_randwaifu_desc"))
-async def getrandwaifu(ctx: discord.Interaction):
+
+@tree.command(name="randwaifu", description=get_string("command_randwaifu_desc"))
+async def get_randwaifu(ctx: discord.Interaction):
     await randwaifu.slash_command_listener(ctx)
 
-# convert image to gif (kinda)
-@tree.command(name="create_gif", description = get_string("command_create_gif_desc"))
+
+@tree.command(name="create_gif", description=get_string("command_create_gif_desc"))
 async def create_gif(ctx: discord.Interaction, file: discord.Attachment):
     await creategif.slash_command_listener(ctx, file)
 
-# Bean user
+
 @tree.command(name="bean", description=get_string("embed_desc", "bean"))
 async def bean_user(ctx: discord.Interaction, user: discord.User, reason: str):
     await bean.slash_command_listener(ctx, user, reason)
 
-# Get bot prefix
+
 @tree.command(name="get_prefix", description="Lấy prefix của con bot tại server hiện tại")
 async def get_bot_prefix(ctx: discord.Interaction):
     await getprefix.slash_command_listener(ctx)
 
+
 # On ready event
 @client.event
 async def on_ready():
-    #tree.clear_commands(guild = None) # Uncomment this to clear all commands
+    # tree.clear_commands(guild = None) # Uncomment this to clear all commands
     await tree.sync()
     await features.onready_things.on_ready(client)
+
 
 # On message delete event
 @client.event
 async def on_message_delete(message):
-    # Ghost ping detector 6900
     await features.ghostping_detector.on_delete(message)
+
 
 # On message edit event
 @client.event
 async def on_message_edit(before, after):
-    # Ghost ping detector 6900
-    await features.ghostping_detector.on_edit(before,after)
+    await features.ghostping_detector.on_edit(before, after)
+
 
 # On message event
 @client.event
 async def on_message(message: discord.Message):
-    
     # log console
     if message.author == client.user:
-        print(">Bot:",message.content,"\n")
-        return   
-    
+        print(">Bot:", message.content, "\n")
+        return
+
     userid = str(message.author.id)
     username = message.author.global_name
     prefix = get_prefix(message.channel.guild)
 
     # If someone use command
     if message.content.startswith(prefix):
-        
+
         # log console
-        print(f"{message.author.global_name} at #{message.channel} on {message.guild} : {message.content}")  
-        
+        print(f"{message.author.global_name} at #{message.channel} on {message.guild} : {message.content}")
+
         # bot user can not use this bot's commands
         if message.author.bot:
             if message.author != client.user:
                 await message.channel.send(get_string("bot_use_command_prompt"))
                 return
-        
+
         if int(userid) in config.banned_users:
             await message.channel.send(get_string("banned_user_prompt"))
             return
-        
+
         # Get requested command
-        command = message.content.split()[0].replace(prefix,'')
-        
-        plain_args = message.content[len(prefix+command)+1:]
+        command = message.content.split()[0].replace(prefix, '')
+
+        plain_args = message.content[len(prefix + command) + 1:]
         args = plain_args.split()
-        
+
         # Main thing
         match command:
-            
-            # Debug
+
             case 'debug':
-                await message.channel.send(f"user_id: {message.author.id}, channel_id: {message.channel.id}, guild: {message.guild}")
-            
-            # Get loca string
-            case 'getloca':    
+                await message.channel.send(
+                    f"user_id: {message.author.id}, channel_id: {message.channel.id}, guild: {message.guild}")
+
+            case 'getloca':
                 await message.channel.send(get_string_by_id(f"loca/loca - {args[0]}.csv", args[1], args[2]))
 
-            # Help
             case 'help':
                 await bot_help.command_listener(message)
-            
-            # Ping pong ping pong
+
             case 'ping':
                 await ping.command_listener(message, client)
 
-            # Echo
             case 'echo':
                 await echo.command_listener(message, plain_args)
-            
-            # Pick
+
             case 'pick':
                 await pick.command_listener(message, plain_args)
-            
-            # Random caps
+
             case 'randcaps':
-                await randcaps.command_listener(message, plain_args)  
-            
-            # osu!
+                await randcaps.command_listener(message, plain_args)
+
             case 'osu':
                 await osu.command_listener(message, plain_args)
-            
-            # emoji
+
             case 'emoji':
                 await getemoji.command_listener(message, client, args)
-            
-            # Ask
+
             case 'ask':
                 await ask.command_listener(message, plain_args)
-            
-            # Nijika
+
             case 'nijika':
                 await nijika.command_listener(message)
 
-            # Amogus
             case 'amogus':
                 await amogus.command_listener(message)
-            
-            # Gvs
+
             case 'gvs':
                 await gvs.command_listener(message, args)
+
             # Invalid command
             case _:
                 await message.channel.send(get_string("command_not_found_prompt"))
-           
-    else:
-        # On bot mentioned
-        await features.on_bot_mentioned.reply(client, message)
 
-        # gvs
+    else:
+        await features.on_bot_mentioned.reply(client, message)
         await features.gvscount.gvs(message, userid, username)
-        
-        # Auto react emojis
         await features.auto_react_emoji.react(autoreact_emojis, message)
+
 
 client.run(TOKEN)
