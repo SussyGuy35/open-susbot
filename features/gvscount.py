8d79ae18a92996b21a5c12e2d3f02a3913c5b1ff
SussyGuy35 <tqbach184@gmail.com>
2024-05-30 11:23:00 +0700

    i forgo bout gvs counter

--- features/gvscount.py
+++ features/gvscount.py
@@ -1,10 +1,15 @@
 import discord
+try:
+    import config_override as config
+except:
+    import config
 import commands.gvs as gkevaysao
+
 
 
 async def gvs(message: discord.Message, userid: str, username: str):
     if message.author.bot:
         return
-    if message.channel.type == discord.ChannelType.text or message.channel.type == discord.ChannelType.voice:
+    if message.channel.type in config.autoreact_emojis_supported_channel_types:
         if "gvs" in message.content.lower():
             gkevaysao.gvs(userid, username, str(message.guild.id))
