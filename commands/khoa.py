9d0d742fd74ea85a774e68550579ea952aeedb91
Saki Toshimiya <166310923+Sakitoshimiya@users.noreply.github.com>
2024-06-16 14:56:38 +0700

    Update khoa.py
    xóa phần print và thêm phần báo user khi không tìm ra

--- commands/khoa.py
+++ commands/khoa.py
@@ -10,11 +10,14 @@ def search_files(path, name):
 
 async def search_khoa(q, ctx: discord.Interaction):
     q = q.replace(" ","_")
-    print(q)
     directory_path = 'khoa_search//'
     matching_files = search_files(directory_path, q)
-    await ctx.response.defer()
-    await ctx.followup.send(file=discord.File(f'khoa_search/{matching_files}'))
+    if matching_files != None:
+        await ctx.response.defer()
+        await ctx.followup.send(file=discord.File(f'khoa_search/{matching_files}'))
+    else:
+        await ctx.response.defer()
+        await ctx.followup.send(file=discord.File('Ăng khoa chưa nói câu nào như thế!'))
 
 def command_response():
     return discord.File(f'khoa_quote/k_{random.randint(1,35)}.jpg')
