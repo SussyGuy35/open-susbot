2b5471df8d6eb274c273da9f231c071341034785
Saki Miya <sakitoshimiya@gmail.com>
2024-06-16 15:33:18 +0700

    khoa

--- commands/khoa.py
+++ commands/khoa.py
@@ -4,7 +4,7 @@ from lib.sussyutils import pick_random_file_from_dir
 import lib.cmddata as cmddata
 
 img_rand_path = cmddata.get_path("khoa/khoa_quote/")
-img_rand_path = cmddata.get_path("khoa/khoa_search/")
+img_search_path = cmddata.get_path("khoa/khoa_search/")
 
 def search_files(path, name):
     for (root,dirs, files) in os.walk(path):
@@ -16,11 +16,10 @@ async def search_khoa(q, ctx: discord.Interaction):
     q = q.replace(" ","_")
     directory_path = 'khoa_search//'
     matching_files = search_files(directory_path, q)
+    await ctx.response.defer()
     if matching_files != None:
-        await ctx.response.defer()
-        await ctx.followup.send(file=discord.File(f'khoa_search/{matching_files}'))
+        await ctx.followup.send(file=discord.File(img_search_path + matching_files))
     else:
-        await ctx.response.defer()
         await ctx.followup.send(file=discord.File('Ăng khoa chưa nói câu nào như thế!'))
 
 def command_response():
