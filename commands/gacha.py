1152c8780958b07497b658776bdceb931c7891b2
SussyGuy35 <tqbach184@gmail.com>
2024-07-21 20:05:26 +0700

    return gacha part 3: leaderboard, rps

--- commands/gacha.py
+++ commands/gacha.py
@@ -3,6 +3,7 @@ import csv
 import json
 import random
 import datetime
+import enum
 import lib.sussyutils as sussyutils
 from lib.locareader import get_string_by_id
 from lib.sussyconfig import get_config
@@ -13,7 +14,14 @@ config = get_config()
 CMD_NAME = "gacha"
 loca_sheet = f"loca/loca - {CMD_NAME}.csv"
 card_database_path = cmddata.get_res_file_path("gacha/carddb.csv")
-save_data_path = "gacha.json"
+save_data_path = "gacha_data.json"
+leaderboard_path = "gacha_leaderboard.json"
+
+
+class RpsClass(enum.Enum):
+    Rock = get_string_by_id(loca_sheet, "rps_rock", config.language)
+    Paper = get_string_by_id(loca_sheet, "rps_paper", config.language)
+    Scissors = get_string_by_id(loca_sheet, "rps_scissors", config.language)
 
 # MARK: some things
 maxiumum_gacha_pull = 10
@@ -23,13 +31,14 @@ bonus_money_when_pull_same_card = int(roll_price/4)
 bonus_credit_when_level_up = 200
 daily_bonus = 100
 newbie_bonus = 1500
+rps_min_bet = 30
+rps_max_bet_multiplier = 200
 
 # MARK: Data
 try:
     data = json.load(cmddata.file_save_open_read(save_data_path))
 except:
     data = {}
-
 
 def save():
     file = cmddata.file_save_open_write(save_data_path)
@@ -98,6 +107,10 @@ def exp_to_advance_level(level: int) -> int:
     return 150*level + 10*(level-1)**3
 
 
+def rps_bonus_exp(bet: int) -> int:
+    return round(0.07*bet)
+
+
 async def check_user_level_up(userid: str | int, channel: discord.TextChannel):
     userid = str(userid)
     if userid not in data.keys():
@@ -117,7 +130,50 @@ async def check_user_level_up(userid: str | int, channel: discord.TextChannel):
             total_bonus_money,
             total_bonus_guarantee
         ))
-        
+
+
+def get_leaderboard() -> dict[str, str]:
+    try:
+        leaderboard = json.load(cmddata.file_save_open_read(leaderboard_path))
+    except:
+        process_leaderboard()
+        return get_leaderboard()
+    
+    return leaderboard
+
+
+def save_leaderboard(leaderboard: dict[str, str]):
+    with cmddata.file_save_open_write(leaderboard_path) as file:
+        json.dump(leaderboard, file)
+
+
+def process_leaderboard():
+    # colect all user data
+    temp = {}
+    for user in data.keys():
+        if user not in temp.keys():
+            temp[user] = get_user_data(user, "exp")
+    # sort by exp
+    temp = {userid: exp for userid, exp in sorted(temp.items(), key=lambda item: item[1], reverse=True)}
+    # create leaderboard
+    leaderboard = {}
+    rank = 1
+    for userid in temp.keys():
+        leaderboard[rank] = userid
+        rank += 1
+    save_leaderboard(leaderboard)
+
+
+def get_user_rank(userid: str | int) -> int:
+    userid = str(userid)
+    if userid not in data.keys():
+        create_user(userid)
+    leaderboard = get_leaderboard()
+    for rank in leaderboard.keys():
+        if leaderboard[rank] == userid:
+            return rank
+    return None
+
 
 # MARK: Card Database
 
@@ -137,6 +193,15 @@ def get_card_name_by_id(id:str) -> str:
         for row in csv_reader:
             if row["card_id"] == id:
                 return row["card_name"]
+        return None
+    
+
+def get_card_id_by_name(name:str) -> str:
+    with open(card_database_path, encoding="utf8") as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            if row["card_name"] == name:
+                return row["card_id"]
         return None
 
 
@@ -202,6 +267,37 @@ def get_bonus_exp_by_rarity(rarity: str) -> int:
             return 0
 
 
+def card_rarity_rank(class_name: str) -> int:
+    match class_name:
+        case "Limited":
+            return 6
+        case "Legendary":
+            return 5
+        case "Epic":
+            return 4
+        case "Rare":
+            return 3
+        case "Uncommon":
+            return 2
+        case "Common":
+            return 1
+        case _:
+            return 0
+        
+
+def card_class_to_enum(class_name: str) -> RpsClass:
+    match class_name:
+        case "rock":
+            return RpsClass.Rock
+        case "paper":
+            return RpsClass.Paper
+        case "scissors":
+            return RpsClass.Scissors
+        case _:
+            return None
+
+
+# MARK: Command Response
 def command_response(args: list[str], user: discord.User, bot: discord.Client) -> str | discord.Embed:
     args_len = len(args)
     
@@ -397,6 +493,10 @@ def command_response(args: list[str], user: discord.User, bot: discord.Client) -
             value=exp_to_advance_level(get_user_data(user_to_show.id, "level"))-get_user_data(user_to_show.id, "exp")
         )
         response.add_field(
+            name=get_string_by_id(loca_sheet, "userinfo_rank", config.language),
+            value=f"#{get_user_rank(user_to_show.id)}"
+        )
+        response.add_field(
             name=get_string_by_id(loca_sheet, "userinfo_credit", config.language),
             value=get_user_data(user_to_show.id, "money")
         )
@@ -408,8 +508,167 @@ def command_response(args: list[str], user: discord.User, bot: discord.Client) -
             name=get_string_by_id(loca_sheet, "userinfo_roll", config.language),
             value=get_user_data(user_to_show.id, "roll")
         )
+        if len(get_user_data(user_to_show.id, "badges")) > 0:
+            response.add_field(
+                name=get_string_by_id(loca_sheet, "userinfo_badges", config.language),
+                value="\n".join([f"- {badge}" for badge in get_user_data(user_to_show.id, "badges")])
+            )
         response.set_thumbnail(url=user_to_show.display_avatar.url)
         return response
+    # endregion
+
+    # region leaderboard
+    elif args[0] == "lb":
+        leaderboard = get_leaderboard()
+        if len(leaderboard) == 0:
+            return get_string_by_id(loca_sheet, "leaderboard_empty", config.language)
+        
+        response = discord.Embed(
+            title=get_string_by_id(loca_sheet, "leaderboard_prompt", config.language),
+            color=0x00ff00
+        )
+
+        for rank in range(1, min(11, len(leaderboard)+1)):
+            user = bot.get_user(int(leaderboard[str(rank)]))
+            response.add_field(
+                name=f"#{rank} - {user.display_name}",
+                value=f"Level: {get_user_data(user.id, 'level')} | Exp: {get_user_data(user.id, 'exp')}",
+                inline=False
+            )
+
+        return response
+    # endregion
+
+    # region rps
+    elif args[0] == "rps":
+        if args_len < 2:
+            return get_string_by_id(loca_sheet, "invalid_bet_point", config.language).format(
+                rps_min_bet,
+                rps_max_bet_multiplier*get_user_data(user.id, "level")
+            )
+        try:
+            bet = int(args[1])
+        except:
+            return get_string_by_id(loca_sheet, "invalid_bet_point", config.language).format(
+                rps_min_bet,
+                rps_max_bet_multiplier*get_user_data(user.id, "level")
+            )
+        
+        if bet < rps_min_bet or bet > rps_max_bet_multiplier*get_user_data(user.id, "level"):
+            return get_string_by_id(loca_sheet, "invalid_bet_point", config.language).format(
+                rps_min_bet,
+                rps_max_bet_multiplier*get_user_data(user.id, "level")
+            )
+        if get_user_data(user.id, "money") < bet:
+            return get_string_by_id(loca_sheet, "rps_cant_afford", config.language)
+        
+        if args_len < 3:
+            return get_string_by_id(loca_sheet, "missing_card_name", config.language)
+        
+        card_to_play = args[2]
+        if not get_card_id_by_name(card_to_play):
+            return get_string_by_id(loca_sheet, "non_existent_card", config.language)
+        if not get_card_id_by_name(card_to_play) in get_user_data(user.id, "cards"):
+            return get_string_by_id(loca_sheet, "invalid_card", config.language)
+
+        bot_card = get_random_card_by_rarity(get_card_roll_rarity(5,20,25,30,20))
+
+        user_card_class = card_class_to_enum(get_card_class_by_id(get_card_id_by_name(card_to_play)))
+        bot_card_class = card_class_to_enum(get_card_class_by_id(bot_card))
+        user_card_rank = card_rarity_rank(get_card_rarity_by_id(get_card_id_by_name(card_to_play)))
+        bot_card_rank = card_rarity_rank(get_card_rarity_by_id(bot_card))
+
+        match user_card_class:
+            case RpsClass.Rock:
+                if bot_card_class == RpsClass.Rock:
+                    result = "draw"
+                elif bot_card_class == RpsClass.Paper:
+                    result = "lose"
+                elif bot_card_class == RpsClass.Scissors:
+                    result = "win"
+            case RpsClass.Paper:
+                if bot_card_class == RpsClass.Rock:
+                    result = "win"
+                elif bot_card_class == RpsClass.Paper:
+                    result = "draw"
+                elif bot_card_class == RpsClass.Scissors:
+                    result = "lose"
+            case RpsClass.Scissors:
+                if bot_card_class == RpsClass.Rock:
+                    result = "lose"
+                elif bot_card_class == RpsClass.Paper:
+                    result = "win"
+                elif bot_card_class == RpsClass.Scissors:
+                    result = "draw"
+            case _:
+                result = "draw"
+        
+        if result == "win":
+            set_user_data(user.id, "money", get_user_data(user.id, "money") + bet)
+            set_user_data(user.id, "exp", get_user_data(user.id, "exp") + rps_bonus_exp(bet))
+            if not bot_card in get_user_data(user.id, "cards"):
+                add_card_to_user(user.id, bot_card)
+                return get_string_by_id(loca_sheet, "rps_win", config.language).format(
+                    get_card_name_by_id(bot_card),
+                    user_card_class.value,
+                    bot_card_class.value,
+                    bet,
+                    rps_bonus_exp(bet)
+                )
+            else:
+                return get_string_by_id(loca_sheet, "rps_win_already_have", config.language).format(
+                    get_card_name_by_id(bot_card),
+                    user_card_class.value,
+                    bot_card_class.value,
+                    bet,
+                    rps_bonus_exp(bet)
+                )
+        
+        elif result == "lose":
+            set_user_data(user.id, "money", get_user_data(user.id, "money") - bet)
+            remove_card_from_user(user.id, get_card_id_by_name(card_to_play))
+            return get_string_by_id(loca_sheet, "rps_lose", config.language).format(
+                get_card_name_by_id(bot_card),
+                user_card_class.value,
+                bot_card_class.value,
+                bet,
+                card_to_play
+            )
+
+        elif result == "draw":
+            if user_card_rank > bot_card_rank:
+                set_user_data(user.id, "money", get_user_data(user.id, "money") + bet)
+                set_user_data(user.id, "exp", get_user_data(user.id, "exp") + rps_bonus_exp(bet))
+                if not bot_card in get_user_data(user.id, "cards"):
+                    add_card_to_user(user.id, bot_card)
+                    return get_string_by_id(loca_sheet, "rps_tie_win", config.language).format(
+                        get_card_name_by_id(bot_card),
+                        user_card_class.value,
+                        bet,
+                        rps_bonus_exp(bet)
+                    )    
+                else:
+                    return get_string_by_id(loca_sheet, "rps_tie_win_already_have", config.language).format(
+                        get_card_name_by_id(bot_card),
+                        user_card_class.value,
+                        bet,
+                        rps_bonus_exp(bet)
+                    )
+            
+            elif user_card_rank < bot_card_rank:
+                set_user_data(user.id, "money", get_user_data(user.id, "money") - bet)
+                remove_card_from_user(user.id, get_card_id_by_name(card_to_play))
+                return get_string_by_id(loca_sheet, "rps_tie_lose", config.language).format(
+                    get_card_name_by_id(bot_card),
+                    user_card_class.value,
+                    bet,
+                    card_to_play
+                )
+            else:
+                return get_string_by_id(loca_sheet, "rps_tie_tie", config.language).format(
+                    get_card_name_by_id(bot_card),
+                    user_card_class.value
+                )
     # endregion
 
 
@@ -424,3 +683,4 @@ async def command_listener(message: discord.Message, args: list[str], bot: disco
         await message.reply(response, mention_author=False)
     
     await check_user_level_up(message.author.id, message.channel)
+    process_leaderboard()
