2bbfc9f16bb9aa276a5eb56961275b5681592d50
SussyGuy35 <tqbach184@gmail.com>
2025-10-08 17:54:06 +0700

    encrypt&decrypt: cleanup and add help entry

--- commands/decrypt.py
+++ commands/decrypt.py
@@ -1,5 +1,4 @@
 import discord
-import random
 import string
 from lib.locareader import get_string_by_id
 import lib.sussyhelper as sh
@@ -17,16 +16,31 @@ chars = list(chars)
 key = ['ỵ', '^', 'Ẵ', 'ớ', 'Ờ', 'ệ', 'ụ', 'Ỹ', 'ù', 'ộ', 'Ă', 'P', 'Ủ', 'Õ', 'F', 'Ĩ', 'Ẫ', 'Ổ', 'ỗ', 'Ở', 'G', 'Ả', 'S', 'á', 'ố', '=', 'Ọ', 'Đ', 'ì', 'Ể', 'ể', '"', 'ả', 'Ự', 'v', 'Z', 'ắ', 'J', '!', 'q', 'ử', 'u', 'ò', 'Ỏ', 'm', 'ỳ', 'j', '#', '%', '6', '7', 'Ữ', 'Ụ', 'Ộ', 'B', 'ị', 'i', 'Ẩ', 'ỹ', 'C', 'ỡ', 'r', 'ý', ']', '.', 'í', 'ỏ', 'Q', 'ặ', 'z', '8', 'Ý', 'Ắ', 'Ị', 'ẵ', ')', 'w', 'T', 'b', 'Ễ', 'a', 'ừ', 'Ẹ', 'Ừ', '[','\n' , 'p', 't', 'Ì', 'ạ', 'W', 'ễ', 'f', 'ỉ', 'U', 'n', '$', '1', 'Ú', 'ọ', 'Ẻ', 'R', 'ơ', 'L', 'g', 'ề', 'Ỉ', 'ủ', 'A', 'ấ', 'ô', 'Ẽ', 'H', 'ầ', 'Ứ', 'è', '+', ',', 'Í', 'Ũ', 'ậ', 'ư', ' ', 'D', 'ó', '3', 'Ỷ', 'ằ', 'ợ', 'À', 'Ế', '-', 'l', '\\', 'o', 'ứ', 'd', 'ú', 'Ử', '2', 'I', 'Ệ', '|', 'â', 'È', 'ồ', 'ũ', 'e', 'K', 'Y', 'c', 'Ề', '5', 'ẻ', 'Ỡ', 'k', 'Â', 'Ã', 'É', 'Á', 'Ô', 'Ằ', 'Ặ', 'ê', '*', 'Ầ', 'ẩ', 'ã', "'", 'ữ', 'Ỳ', 'Ấ', '&', '}', ':', '>', 'Ẳ', 'Ỵ', 'O', 'Ò', 'ẽ', '{', '4', 's', 'Ớ', 'Ơ', 'N', 'h', '~', '9', 'õ', 'x', 'Ợ', 'Ạ', 'ờ', 'ẹ', 'ĩ', 'đ', 'y', 'X', 'E', 'Ư', '/', 'Ê', 'ỷ', 'ở', 'ự', 'Ó', '?', 'Ỗ', 'M', '0', 'ă', 'ẫ', 'Ố', '(', 'ổ', 'ẳ', 'Ù', '_', ';', 'Ậ', '@', '`', 'é', 'Ồ', 'V', 'à', '<', 'ế']
 
 
+sh.HelpManager.add_command_help(
+    sh.CommandHelp(
+        command_name=cmd_names[0],
+        command_type=sh.CommandType.PREFIX,
+        description=get_string_by_id(loca_sheet, "command_desc"),
+        usage=get_string_by_id(loca_sheet, "command_usage"),
+        aliases=cmd_names[1:],
+        parameters=[
+            sh.CommandParameterDescription(
+                name="message",
+                description=get_string_by_id(loca_sheet, "command_param_message_desc"),
+                required=True
+            )
+        ]
+    ),
+    sh.HelpSection.GENERAL2
+)
 
-
-    
 
 def command_response(msg: str):
     decipher_text = ""
     for letter in msg:
         index = key.index(letter)   
         decipher_text += chars[index]      
-        decipher_text = decipher_text.replace("@everyone", "`@everyone`").replace("@here", "`@here`")
+    decipher_text = decipher_text.replace("@everyone", "`@everyone`").replace("@here", "`@here`")
 
     return decipher_text
 
@@ -34,7 +48,7 @@ async def command_listener(message: discord.Message, msg: str):
 
 
     if any(filterletter not in key for filterletter in msg):
-        await message.channel.send("không hỗ trợ mã hoá các kí tự đặc biệt hay những kí tự nước ngoài, emoji, ..., hãy thử xoá những kí tự ấy nhé ")
+        await message.channel.send(get_string_by_id(loca_sheet, "invalid_message"))
     else:
         await message.channel.send(command_response(msg))
 
