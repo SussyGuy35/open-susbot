20ed4113d126179b9444fab32a9798d848df0807
SussyGuy35 <tqbach184@gmail.com>
2025-12-15 11:48:19 +0700

    iuwerow4nfejiwhefwer

--- features/gvscount.py
+++ features/gvscount.py
@@ -1,13 +1,13 @@
 import discord
-from lib.sussyconfig import get_config
 import commands.gvs as gkevaysao
+from features.auto_react_emoji import supported_channel_types as autoreact_emojis_supported_channel_types
+import config
 
-config = get_config()
 
 
 async def gvs(message: discord.Message, userid: str):
     if message.author.bot:
         return
-    if message.channel.type in config.autoreact_emojis_supported_channel_types:
+    if message.channel.type in autoreact_emojis_supported_channel_types:
         if "gvs" in message.content.lower():
             gkevaysao.gvs(userid, str(message.guild.id))
